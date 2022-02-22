29ae6b8f26679a0072c4a6c56c387f80cddbc233
Castorprince <99782655+Castorprince@users.noreply.github.com>
2022-02-21 21:37:40 -0500

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#file is ready for deployment
+echo "please enter your name"
+read name
+echo "how old are you ?"
+read age
+echo "please enter your password"
+read -s password
+echo "welcome to TD bank"
